576964a38f4a69e8d1699184a56ca3061b0623d3
shikky0331 <shikky0335@gmail.com>
2018-12-06 11:56:50 +0900

    card_show_edit_update_destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root "top#index"
 
 
-  resources :list, only: %i(new create edit update destroy)
-
+  resources :list, only: %i(new create edit update destroy) do
+    resources :card, except: %i(index)
+  end
 end
